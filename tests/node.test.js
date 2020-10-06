d958adab941bf5be3a3ca7adcce2317e9e528b72
Taylor Keller <takeller17@gmail.com>
2020-10-06 09:29:29 -0600

    Project setup and first test file

---
+++ tests/node.test.js
@@ -0,0 +1,25 @@
+// Node.new 
+// node.data
+// node.next_node
+
+describe('Node', () => {
+
+    beforeAll(() => {
+        let node1 = new Node("Hello")
+        let node2 = new Node("world!")
+    });
+
+    it('Creates a new node', () => {
+        expect(node1).toBeInstanceOf(Node);
+    });
+
+    it('Can access a nodes data', () => {
+        expect(node1.data).toEqual("Hello")
+        expect(node2.data).toEqual("world!")
+    });
+
+    it('Can access pointer to the next node', () => {
+        expect(node1.next_node).toEqual(Node2)
+        expect(node2.next_node).toEqual(null)
+    });
+})
